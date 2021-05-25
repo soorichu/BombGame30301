f37059ca9b00dc68d9e1dd13be5879abd2b12763
Soori <soorichu@gmail.com>
2021-05-25 12:18:25 +0900

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,6 +36,13 @@ public class PlayerController : MonoBehaviour
         if (collision.collider.tag == "Obstacle")
         {
             ani.SetTrigger("damage");
+            GameManager.instance.AddScore(-1);
+        }
+
+        if (collision.collider.tag == "Enermy")
+        {
+            ani.SetTrigger("attack01");
+            GameManager.instance.OnPlayerDead();
         }
     }
 }
